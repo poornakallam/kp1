8f0cbae5c2b042df8687f3212a67b5f29c660e70
Poorna Kallam <2233826@tcs.com>
2025-07-03 13:08:25 +0530

    py

--- python/strings.py
+++ python/strings.py
@@ -3,3 +3,12 @@ print(A[10])
 print(A[5:7])
 print(A[8:])
 print(A[:4])
+
+#Get the string slice "do it!" from the variable and concatenate it with the string "Don't ".  
+#Print the resulting string, which should be "Don't do it!" where the "do it!" part is a slice.
+
+# Concatenate means it ignore the words between plus(+)
+# key="D" + "-" + "R"
+# prints D-R as results
+
+print("Don't" + A[5:])
