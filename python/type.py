f5d01a414e80bd4353fede49682c169cc5e6ef45
Poorna Kallam <2233826@tcs.com>
2025-07-04 10:58:21 +0530

    type.py

--- python/type.py
+++ python/type.py
@@ -4,11 +4,11 @@ C=True
 D=str(False)
 
 print(type(A))
-print(str(A) + "is int")
+print(str(A) +  "is int")
 print(type(B))
 print(type(C))
 print(type(D))
 
 print("\"Hello, I'm \'poorna\'! glad to meet you\"\nwanted to share info\tmust help you\\")
 
-print("*****\n ****\n ***\n **\n *")
+print("*****\n *** \n * ")
