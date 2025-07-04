046c6f383e18979bba67e758379f1f695dbf133c
Poorna Kallam <2233826@tcs.com>
2025-07-04 21:50:19 +0530

    inputs

--- python/input.py
+++ python/input.py
@@ -1,7 +1,7 @@
-name=input(what is your name?)
-quest=input("enter quest?")
-colour=input("enter colour")
+name=input("what is your name?")
+#quest=input("enter quest?")
+#colour=input("enter colour")
 
 print("my name is" + name + "and favourate colour is" + colour)
-print("my quest is" + quest)
+#print("my quest is" + quest)
 #print("my colour is" + colour)
