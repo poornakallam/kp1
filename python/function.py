fd09a9b9ce896afb3a31305ca406d89d67587a07
Poorna Kallam <2233826@tcs.com>
2025-07-09 13:35:48 +0530

    ..

--- python/function.py
+++ python/function.py
@@ -1,4 +1,21 @@
+l=30
+b=20
+w=10
+
+
+def hello():
+    print(hello world)
+
+hello()    
+
 def name_printer(p):
     print(p + 2)
 
 name_printer(7)
+
+def rectanguler(l, b, w)
+    print(l * b * w)
+
+rectanguler()
+
+
