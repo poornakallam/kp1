a783340500e5ec227ef4c367eb64485d769b8dce
Poorna Kallam <2233826@tcs.com>
2025-07-09 16:14:15 +0530

    ..

--- python/function01.py
+++ python/function01.py
@@ -8,7 +8,7 @@ print("The farenheat is" + str(farenhit(celisus)) + "degrees.")
 
     
 def round_f(cel):
-    return round((cel * 10 + 32), 1)
+    return round((cel * 2 + 32), 1)
 
 print(str(round_f(celisus)))
 
