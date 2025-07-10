05f301c1b5de31f1fd3cdf8876888acb1f924503
Poorna Kallam <2233826@tcs.com>
2025-07-10 09:45:26 +0530

    module

--- python/module.py
+++ python/module.py
@@ -1,4 +1,10 @@
 import random
 from random import randint
-print(random.randint(1, 10))
+#print(random.randint(1, 10))
+
+fuel=randint(10, 20)
+miles=randint(200, 400)
+
+print(str(miles // fuel))
+
 
