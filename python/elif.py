ecdc07cfb922f28b7c96fe5be5fe6d8ac215498a
Poorna Kallam <2233826@tcs.com>
2025-07-16 13:32:37 +0530

    if statements

--- python/elif.py
+++ python/elif.py
@@ -6,3 +6,13 @@ elif fruit == "mango":
     print("the fruit is mango")
 else:
     print("the fruit is neither mango or apple")
+
+number=9
+
+if number <= 10:
+    print("the number is below 10 and number is" + str(number))
+elif number == 10:
+    print("the number is"  + str(number))
+else:    
+    print("the number is above 10 and number is"  + str(number))
+   
