03df9176f3de30e15c214aea2bca24d2a90310e5
Poorna Kallam <2233826@tcs.com>
2025-07-16 13:20:49 +0530

    if statements

--- python/flow.py
+++ python/flow.py
@@ -1,15 +1,17 @@
-# score=int("70")
-# if score>=90:
-#    print("score" + "is an A.") 
-# else:
-#     if score>=80:
-#        print("score" + "is a B.")
-#     else:
-#         if score>=70:
-#            print(str(score) + "is a C.")
-#         else:
-#             print("score" + "is a D.")
+# if statements with single variable
+score=int("70")
+if score>=90:
+   print("score" + "is an A.") 
+else:
+    if score>=80:
+       print("score" + "is a B.")
+    else:
+        if score>=70:
+           print(str(score) + "is a C.")
+        else:
+            print("score" + "is a D.")
 
+#nested if statements( verify multiple variables)
 student=95
 university="no"
 
