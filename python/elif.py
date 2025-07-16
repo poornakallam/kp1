f858741cc71c065f8b99c408b9ff9fd67a2c5b9b
Poorna Kallam <2233826@tcs.com>
2025-07-16 13:24:36 +0530

    if statements

---
+++ python/elif.py
@@ -0,0 +1,8 @@
+fruit="apple"
+
+if fruit == "apple":
+    print("the fruit is apple")
+elif fruit == "mango":
+    print("the fruit is mango")
+else:
+    print("the fruit is neither mango or apple")
