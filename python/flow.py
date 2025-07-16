c03fc3e49404012a91294c51b9ff680d52931a5a
Poorna Kallam <2233826@tcs.com>
2025-07-16 12:40:06 +0530

    if statements

---
+++ python/flow.py
@@ -0,0 +1,11 @@
+score=int("the score is 93")
+if A>=90:
+   print("score" + "is an A.") 
+else:
+    if B>=80:
+       print("score" + "is a B.")
+    else:
+        if C>=70:
+           print("score" + "is a C.")
+        else:
+            print("score" + "is a D.")
