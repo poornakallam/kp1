6179c49a460ed7b9000b148d169c8aec52565fc0
Poorna Kallam <2233826@tcs.com>
2025-07-22 11:12:52 +0530

    while

--- python/while.py
+++ python/while.py
@@ -1,15 +1,21 @@
-counter=0
-while counter < 3:
-    print("works")
-    counter += 1
+# counter=0
+# while counter < 3:
+#     print("works")
+#     counter += 1
 
-word="apple"
+# word="apple"
 
-for num in word:
-    print(num)
+# for num in word:
+#     print(num)
 
-number=range(30, 5, -5)
+# number=range(30, 5, -5)
 
-for num in number:
-    print(num)
-    print(type(number))
+# for num in number:
+#     print(num)
+#     print(type(number))
+
+number=10
+
+while number != 0:
+    print("number")
+    number -= 1    
