6c4121929ad848dbccd765636bea172767efe4cb
Poorna Kallam <2233826@tcs.com>
2025-07-25 10:13:17 +0530

    string

--- python/string.py
+++ python/string.py
@@ -1,5 +1,6 @@
 mixed_var="A song with ice and cold"
 
-print("mixed_var".upper())
-print("mixed_var".isupper())
-print("mixed_var".isalpha())
+print(mixed_var.upper())
+print(mixed_var.isupper())
+print(mixed_var.isalpha())
+print(mixed_var.isspace())
