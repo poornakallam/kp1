4ede0e94ccfbd4e4b061c5c7b18594a96736375a
Poorna Kallam <2233826@tcs.com>
2025-07-25 12:20:04 +0530

    string

--- python/string.py
+++ python/string.py
@@ -1,7 +1,7 @@
 mixed_var="A song with ice and cold"
 name="words"
-words=mixed_var.split(",")
-print("".join([one,two,three]))
+words=mixed_var.split("")
+print("".join(words))
 print(words)
 # print(mixed_var.upper())
 # print(mixed_var.isupper())
