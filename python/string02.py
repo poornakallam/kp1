92b09fd2bc1e56bc7cd4e978e4cf2e1959bf853c
Poorna Kallam <2233826@tcs.com>
2025-07-29 17:03:24 +0530

    string2

--- python/string02.py
+++ python/string02.py
@@ -4,6 +4,6 @@ print(name.rjust(15, "-"))
 print(name.ljust(15, "-"))
 print(name.center(15, "0"))
 
-parameter=("eggs", "milk", "bread", "milk")
+#parameter=("eggs", "milk", "bread", "milk")
 
-print((parameter.rstrip("milk")))
+print("eggs", "milk", "bread", "milk".rstrip("milk"))
