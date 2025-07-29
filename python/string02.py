cdc21b34717d461ea41f2249faf75eaf84aaa625
Poorna Kallam <2233826@tcs.com>
2025-07-29 17:00:23 +0530

    string2

--- python/string02.py
+++ python/string02.py
@@ -2,4 +2,8 @@ name="hello world"
 
 print(name.rjust(15, "-"))
 print(name.ljust(15, "-"))
-print(name.center(15, "p"))
+print(name.center(15, "0"))
+
+parameter=("eggs", "milk", "bread", "milk")
+
+print((rstrip("milk")))
