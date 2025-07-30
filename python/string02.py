968d80441dd9bab9fcf86b989fec9c0099712d4f
Poorna Kallam <2233826@tcs.com>
2025-07-30 12:30:33 +0530

    strip strings

--- python/string02.py
+++ python/string02.py
@@ -1,16 +1,16 @@
 name="hello world"
 
-print(name.rjust(15, "-"))
-print(name.ljust(15, "-"))
-print(name.center(15, "0"))
+# print(name.rjust(15, "-"))
+# print(name.ljust(15, "-"))
+# print(name.center(15, "0"))
 
 #parameter=("eggs", "milk", "bread", "milk")
 
-print("eggs, milk, bread, milk".rstrip(", milk"))
-print("eggs, milk, bread, milk".lstrip(", eggs"))
-print("eggs, milk, bread, milk".rstrip(", ilmk"))
-print("eggs, milk, bread, milk".strip(", milk"))
-print("eggs, milk, bread, milk".replace("bread", "biscuit"))
+# print("eggs, milk, bread, milk".rstrip(", milk"))
+# print("eggs, milk, bread, milk".lstrip(", eggs"))
+# print("eggs, milk, bread, milk".rstrip(", ilmk"))
+# print("eggs, milk, bread, milk".strip(", milk"))
+# print("eggs, milk, bread, milk".replace("bread", "biscuit"))
 
 the_string="North Dakota"
 center_plus=the_string.center(16, "+")
