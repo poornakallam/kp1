08e0d7c178a97dbc7f48612665512acd8ad1bfbc
Poorna Kallam <2233826@tcs.com>
2025-07-30 12:41:44 +0530

    strip strings

--- python/lenstring.py
+++ python/lenstring.py
@@ -2,6 +2,6 @@ user_string = "something123"
 reversed = ""
  
 for item in range(len(user_string) - 1, -1, -1):
-    reversed += user_string[item]
+    reversed = user_string[item]
  
 print(reversed)
