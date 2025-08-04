5c4deddf73b76ad92cfa64f2fd51b80d1ab691de
Poorna Kallam <2233826@tcs.com>
2025-08-04 12:12:16 +0530

    list indexes and slicing

--- python/index_list.py
+++ python/index_list.py
@@ -1,6 +1,6 @@
 list=[[0, 2], [4, 6], [8, 10], [12, 14]]
 print(list[0])
-print(list[4][1])
+print(list[3][1])
 list_items=["chair", "table", "desk", "lamp", "bed"]
 print(list_items[-5])
 #print("Most people own at least 2 \" + list_items[0] + \" "s."")
