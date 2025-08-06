e8f559fc619f67f4cb013927739cf84bbcf65dda
Poorna Kallam <2233826@tcs.com>
2025-08-06 14:38:00 +0530

    basic dictionaries

--- python/list_list.py
+++ python/list_list.py
@@ -1,9 +1,18 @@
-arctic_animals=["penguin", "elephant", "polar bear", "walrus", "tiger", "reindeer"]
-del arctic_animals[4]
-arctic_animals.remove("elephant")
-arctic_animals.append("arctic fox")
-arctic_animals.insert(3, "snowy owl")
-arctic_animals.sort()
-print(arctic_animals)
-print(arctic_animals.index("reindeer"))
-print(arctic_animals.pop())
+# arctic_animals=["penguin", "elephant", "polar bear", "walrus", "tiger", "reindeer"]
+
+# 1, peguin, polar bear, snowy owl, walrus, reindeer
+
+# a, p, p, r, s, w
+# del arctic_animals[4]
+# arctic_animals.remove("elephant")
+# arctic_animals.append("arctic fox")
+# arctic_animals.insert(3, "snowy owl")
+# arctic_animals.sort()
+# print(arctic_animals)
+# print(arctic_animals.index("reindeer"))
+# print(arctic_animals.pop())
+
+animals={"name": "bear", "habbit": "omnivarous", "live" "forest", "lifespan": 9, "family": "yes"}
+print(animals[2])
+print("name" in animals)
+print(lifespan not in animals)
