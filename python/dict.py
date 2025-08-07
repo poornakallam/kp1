d20bdb1bfd4b11686c893b91ba8c9f291c62b47a
Poorna Kallam <2233826@tcs.com>
2025-08-07 12:51:42 +0530

    dictiories

---
+++ python/dict.py
@@ -0,0 +1,12 @@
+kingdom={"Queen": "Bohemian Rhapsody",
+         "Bee Gees": "Stayin' Alive",
+         "U2": "One", 
+         "Michael Jackson": "Billie Jean",
+         "The Beatles": "Hey Jude", 
+         "Bob Dylan": "Like A Rolling Stone"
+        }
+print(len(kingdom))
+print(list(kingdom.keys()))
+print(kingdom.values())
+print(kingdom.items())
+
