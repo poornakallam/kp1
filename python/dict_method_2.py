4e638331a85fa48e48ba71ed3b1608eb4b85765b
Poorna Kallam <2233826@tcs.com>
2025-08-09 06:42:42 +0530

    dict method 2

--- python/dict_method_2.py
+++ python/dict_method_2.py
@@ -3,3 +3,10 @@ consonents={}.fromkeys(["abcd"], "consonents")
 for item in consonents.items():
     print(item)
 print(len(consonents))
+
+fast_food_items = {"McDonald's": "Big Mac", 
+                   "Burger King": "Whopper", 
+                   "Chick-fil-A": "Original Chicken Sandwich"}
+print(fast_food_items.pop("McDonald's"))
+fast_food_items.popitem()
+print(fast_food_items)
