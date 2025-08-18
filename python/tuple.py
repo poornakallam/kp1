3dbc260d360563b92994beaef3cc136f05cb5ffe
Poorna Kallam <2233826@tcs.com>
2025-08-18 12:00:07 +0530

    tuples

--- python/tuple.py
+++ python/tuple.py
@@ -1,10 +1,13 @@
 number=(1,2,3,4,5,6,7,4,5,5,6,6,6,7,8)
-
 print(number[9])
 print(number[::3])
-#3,6,5,6,8
+#1,4,7,5,6
 print(number[2::2])
-#5,7,5,6,6,8
+#3,5,7,5,6,6,8
+print(number[3::2])
+#4,6,4,5,6,7
+print(number[10::-3])
+print(number[::-2])
 print(number.count(6))
-print(number.index(3))
+print(number.index(7))
 
