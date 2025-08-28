b85d22bac0f7885a9a9356ef978df2429b439d88
Poorna Kallam <2233826@tcs.com>
2025-08-28 18:53:15 +0530

    while loop

--- pierian_data_python/while.py
+++ pierian_data_python/while.py
@@ -1,7 +1,19 @@
 x=0
 
 while x < 5:
+    if x == 2:
+        break
     print(x)
     x = x + 1
 else:
     print("nothing")
+
+y="trunk"
+for v in y:
+    if y == "u":
+        continue
+    print(y)
+    if y == "n"
+       pass
+    print(y)
+
