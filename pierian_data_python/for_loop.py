452a5e15f7ddc48761c3ebc3b265b2e6bdd50a5b
Poorna Kallam <2233826@tcs.com>
2025-08-28 16:34:04 +0530

    pierian data for loop

---
+++ pierian_data_python/for_loop.py
@@ -0,0 +1,28 @@
+my_list=[1,2,3,4,5]
+
+my_object="Hello World!"
+
+for num in my_list:
+    print(num)
+    print(cool!)
+
+for name in my_object:
+    print(name)
+    print(cool!)
+
+tup=[(1,2),(3,4),(5,6)]
+
+for numb in tup:
+    print(numb)
+for a,b in tup:
+    print(a)
+    print(b)
+
+dict={"k1": 1, "k2": 2, "k3": 3}
+for item in dict:
+    print(item)
+for key,value in dict:
+    print(key)    
+    print(values)
+for items in dict.items():
+    print(items)
