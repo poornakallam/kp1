41e9ddd3f8b1d29b3dfdd904aaa9b4558ae47f00
Poorna Kallam <2233826@tcs.com>
2025-08-30 11:37:12 +0530

    operators

---
+++ pierian_data_python/operators.py
@@ -0,0 +1,11 @@
+for item in range(1,20,3):
+    print(item)
+
+index_count=0
+for letter in "abcde":
+    print(letter[index_count])
+    index_count += 1
+
+#enumerate
+for items in enumerate("abcde"):
+    print(items)
