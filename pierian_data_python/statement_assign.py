b986892524b2c50f778806eb8b2385917101f34c
Poorna Kallam <2233826@tcs.com>
2025-09-01 12:05:08 +0530

    statements assignment

--- pierian_data_python/statement_assign.py
+++ pierian_data_python/statement_assign.py
@@ -1,33 +1,37 @@
 #Assignment
 #01
-word="Print only the words that start with s in this sentence"
-print(word.split())
-new_word=word.split()
-if new_word[0] == "s":
-    print("new_word")
+st = 'Print only the words that start with s in this sentence'
+for word in st.split():
+    if word[0] == 's':
+        print(word)
 #02    
-list02=range(0,11,2)
-print("list02")
+print(list(range(0,11,2)))
 #03
 numbers=range(1,51)
 for items in numbers:
     if items%3 == 0:
         print(items)
+[x for x in range(1,51) if x%3 == 0]        
 #04
 myword="Print every word in this sentence that has an even number of letters"
-length=len(myword)
-if length%2==0:
-    print("even")
+# if len(myword)%2==0:
+#     print("even")
+for word in myword.split():
+    if len(word)%2 == 0:
+        print(word+" <-- has an even length!")    
 
 #05
-# numbers_range=range(1,100)
-# if numbers_range%3 == 0 & numbers_range%5 == 0:
-#     print("FizzBuss")
-#     if numbers_range%3 == 0:
-#         print("Fizz")
-#         if numbers_range%5 == 0:
-#             print("Buzz")
+for num in range(1,101):
+    if num%3 == 0 and num%5 == 0:
+        print("FizzBuzz")
+    elif num%3 == 0:
+        print("Fizz")
+    elif num%5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+
 #06
-st = 'Create a list of the first letters of every word in this string'
-print(list(st[0][0]))
+words = 'Create a list of the first letters of every word in this string'
+[word[0] for word in words.split()]
 
