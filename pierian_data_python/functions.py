8230e2c10aad11b3e6fd42c09edd35b6071b96f9
Poorna Kallam <2233826@tcs.com>
2025-09-02 15:31:12 +0530

    function basics

--- pierian_data_python/functions.py
+++ pierian_data_python/functions.py
@@ -1,15 +1,20 @@
 #01 - single argument
 def function_name(name):
-    print("Hello" + name)
+    print("Hello".format(name))
 function_name("Josh")
 #02 - multiple arguments
 def numbers(num1, num2):
-    print(num1+num2)
-numbers(10,20)
-#03 - return function
+    print(num1-num2)
+numbers(30,10)
+#03 - return function(not supporting from github)
 def return_nums(a,b):
     return a+b
     results = return_nums(10, 20)
     print(results)
 return_nums('5','15')
-
+#04 - repeating multiple arguments
+def words():
+    print('hello')
+    print('how are you?')
+    print("what's going on")
+words()    
