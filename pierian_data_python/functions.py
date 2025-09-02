85bf17af7e406b7251f024852b9097cadcf50e31
Poorna Kallam <2233826@tcs.com>
2025-09-02 15:20:30 +0530

    function basics

---
+++ pierian_data_python/functions.py
@@ -0,0 +1,14 @@
+#01 - single argument
+def function_name(name):
+    print("Hello" + name)
+function_name("Josh")
+#02 - multiple arguments
+def numbers(num1, num2):
+    print(num1+num2)
+numbers(10,20)
+#03 - return function
+def return_nums(a,b):
+    return a+b
+    results = return_nums(10, 20)
+    print(results)
+
