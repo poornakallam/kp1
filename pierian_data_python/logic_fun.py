1e8aab7d1c159f292c8c7088ccc50034d2de9be6
Poorna Kallam <2233826@tcs.com>
2025-09-03 22:24:20 +0530

    ...

--- pierian_data_python/logic_fun.py
+++ pierian_data_python/logic_fun.py
@@ -2,16 +2,7 @@ print(20%2 == 0)
 def print_even(numbers):
     for num in numbers:
         if num % 2 == 0:
-            print(True)
+            print(num)
         else:
-            print(False)
-print_even([1,2,3,4,5,6])
-
-
-# def numbers(number_even):
-#     for num in number_even:
-#         if num%2 == 0:
-#             print(True)
-#         else:
-#             print(False)
-# numbers(30)
+            print(num)
+print_even([2,3,4,5,6])
