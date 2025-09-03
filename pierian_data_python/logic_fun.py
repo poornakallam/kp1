b8335d7f0f9254fae310631d9d4aea5c8b96ba04
Poorna Kallam <2233826@tcs.com>
2025-09-03 22:31:54 +0530

    logic with function, for and if

--- pierian_data_python/logic_fun.py
+++ pierian_data_python/logic_fun.py
@@ -3,6 +3,16 @@ def print_even(numbers):
     for num in numbers:
         if num % 2 == 0:
             print(num)
-        else:
-            print(num)
+        # else:
+        #     print(num)
 print_even([2,3,4,5,6])
+
+# return statement
+def count_words(words):
+    count = 0
+    for word in words:
+        if word > 3:
+            count += 1
+    return count
+count_words(["hi", "apple", "p", "dog" "banned" ])
+
