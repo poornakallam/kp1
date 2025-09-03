60692c2f4abb72e803c8e646c4129f9652f2c4a2
Poorna Kallam <2233826@tcs.com>
2025-09-03 13:33:31 +0530

    ...

--- pierian_data_python/logic_fun.py
+++ pierian_data_python/logic_fun.py
@@ -1,5 +1,8 @@
 print(20%2 == 0)
 
 def even_number_list(num_list):
-    print(num_list%2 == 0)
-even_number_list([1,2,3,4])
+    return num_list%2 == 0
+    #print(num_list%2 == 0)
+
+nums = [1,2,3,4]
+print(even_number_list(nums))
