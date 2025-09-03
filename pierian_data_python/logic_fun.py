a13a82c008d19912be204157f538270c32c14449
Poorna Kallam <2233826@tcs.com>
2025-09-03 22:03:57 +0530

    ...

--- pierian_data_python/logic_fun.py
+++ pierian_data_python/logic_fun.py
@@ -6,8 +6,8 @@ print(20%2 == 0)
 # print(even_number_list([1,2,3,4]))
 # list and int not working at function variable or argument
 
-def numbers(number):
-    for num in number:
+def numbers(number_even):
+    for num in number_even:
         if num % 2 == 0:
             print(True)
         else:
