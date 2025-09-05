5aacc4b6ec0c0597076fe096e68d0558ba43964a
Poorna Kallam <2233826@tcs.com>
2025-09-05 11:54:30 +0530

    tuple function

--- pierian_data_python/tuple_fun.py
+++ pierian_data_python/tuple_fun.py
@@ -9,5 +9,13 @@ def process_pairs():
     for x,y in pairs:
         if x % 2 == 1:
             print(f"{x} is even, y={y}")
-process_pairs()
+process_pairs() # without closing function print not works at 'if' condition
+
+def subject_marks():
+    marks = [("mon", (85, 95, 90)), ("tue", (80, 75, 65)), ("wed", (70, 65, 50))]
+    for x, (a,b,c) in marks:
+        if a >= 80 and b > 70 and c > 75
+            print(f"{x} is above {a}, {b} and {c}")
+subject_marks()            
+
 
