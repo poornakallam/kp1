c811e961cf3d9e91a269db5c5d1f077d19a37637
Poorna Kallam <2233826@tcs.com>
2025-09-05 11:40:41 +0530

    tuple function

--- pierian_data_python/tuple_fun.py
+++ pierian_data_python/tuple_fun.py
@@ -1,6 +1,13 @@
-# def get_employee():
 employee = [("aaa", 30), ("bbb", 25), ("ccc", 20), ("ddd", 35)]
 
 for name, age in employee:
     if age > 25:
         print(name + " is young")
+def process_pairs():
+    pairs = [(1,2), (3,4), (5,6), (7,8)]
+
+    for x,y in pairs:
+        if x % 2 == 1:
+            print(x " is odd and " + y)
+
+
