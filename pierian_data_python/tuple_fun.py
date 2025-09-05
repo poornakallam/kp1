19f81f83ba03158b4a91124daab8a1c3a08b83b9
Poorna Kallam <2233826@tcs.com>
2025-09-05 11:31:15 +0530

    tuple function

--- pierian_data_python/tuple_fun.py
+++ pierian_data_python/tuple_fun.py
@@ -1,6 +1,6 @@
 # def get_employee():
 employee = [("aaa", 30), ("bbb", 25), ("ccc", 20), ("ddd", 35)]
 
-for name, age in get_employee():
+for name, age in employee:
     if age > 25:
         print(name + "is young")
