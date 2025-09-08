5ba2202d9201c8bc9b92924161808bc9eecae2c9
Poorna Kallam <2233826@tcs.com>
2025-09-08 16:22:50 +0530

    ...

--- pierian_data_python/exercises_functions.py
+++ pierian_data_python/exercises_functions.py
@@ -5,9 +5,15 @@
 #         print(False)        
 # animals("tiger","turtle")
 
-def intergers(x,y):
-    if x+y == 20 or x == 20 or y == 20:
-        print(True)
-    else:
-        print(False)        
-intergers(5,25)      
+# def intergers(x,y):
+#     if x+y == 20 or x == 20 or y == 20:
+#         print(True)
+#     else:
+#         print(False)        
+# intergers(5,25)      
+
+# Level 01
+def myfunc(a):
+    a[0][4].upper()
+results=myfunc("macdonald")
+print(results)
