c1618312ce66ba05138fb369f99900b30073a01c
Poorna Kallam <2233826@tcs.com>
2025-09-08 16:18:14 +0530

    ...

--- pierian_data_python/exercises_functions.py
+++ pierian_data_python/exercises_functions.py
@@ -1,13 +1,13 @@
-def animals(a,b):   
-    if a[0] == b[0]:
-        print(True)
-    else:
-        print(False)        
-animals("tiger","turtle")
+# def animals(a,b):   
+#     if a[0] == b[0]:
+#         print(True)
+#     else:
+#         print(False)        
+# animals("tiger","turtle")
 
 def intergers(x,y):
     if x+y == 20 or x == 20 or y == 20:
         print(True)
     else:
         print(False)        
-intergers(10,10)        
+intergers(20,10)      
