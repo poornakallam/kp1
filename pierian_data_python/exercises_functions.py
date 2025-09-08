dd0d67b3c659cdce04100a3b8c89956c62afc7db
Poorna Kallam <2233826@tcs.com>
2025-09-08 15:56:29 +0530

    ...

--- pierian_data_python/exercises_functions.py
+++ pierian_data_python/exercises_functions.py
@@ -1,7 +1,3 @@
 def animals(a,b):   
-    if animals(a[0]) == animals(b[0]):
-        print(True)
-    else:
-        print(False)
-animals("tiger","turtle")
-
+    a[0] == b[0]
+print(animals("tiger","turtle"))
