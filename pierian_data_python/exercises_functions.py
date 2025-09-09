543fba6ec5ef81d0fe4b87ed499a3b8873dd24f7
Poorna Kallam <2233826@tcs.com>
2025-09-09 11:03:41 +0530

    level 2 functions

--- pierian_data_python/exercises_functions.py
+++ pierian_data_python/exercises_functions.py
@@ -41,7 +41,7 @@
 
 #Level 2
 def hang_33(nums):
-    for i in range(0, len(nums)):
+    for i in range(0, len(nums)-1):
         if nums[i] == 3 and nums[i+1] == 3:
             print(True)
         else:
