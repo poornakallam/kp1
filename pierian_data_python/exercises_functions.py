0c8ac9e4bc723531c886d40e31b12dc8e2ad2de4
Poorna Kallam <2233826@tcs.com>
2025-09-09 12:08:19 +0530

    level 2 functions

--- pierian_data_python/exercises_functions.py
+++ pierian_data_python/exercises_functions.py
@@ -58,9 +58,9 @@ func('hello')
 def intergers(a,b,c):
     if sum([a,b,c]) <= 21:
         return sum([a,b,c])
-    elif sum([a,b,c]) > 21 and 11 in [a,b,c]:
+    elif sum([a,b,c]) <= 31 and 11 in [a,b,c]:
         return sum([a,b,c])-10
     else:
         return "BUST"
-results=intergers(11,6,5)
+results=intergers(11,11,10)
 print(results)
