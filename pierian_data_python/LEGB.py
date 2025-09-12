a3a7d28e3b2047da7669d67d28931e7af43dd71c
Poorna Kallam <2233826@tcs.com>
2025-09-12 18:01:59 +0530

    passing function LEGB rules

---
+++ pierian_data_python/LEGB.py
@@ -0,0 +1,13 @@
+name = "first time" #Global
+
+def parameter():
+    name = "second time" #Enclosing 
+    def params():
+        name = "third time" #Local
+        print("Hello " + name)
+    params()    
+print("Hello " + name)    
+parameter()
+
+print(params)
+print(parameter)
