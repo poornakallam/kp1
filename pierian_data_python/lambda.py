21a2a14c633683eb2df6665fdbd0de45061e6772
Poorna Kallam <2233826@tcs.com>
2025-09-12 12:23:36 +0530

    lambda, map and filter expresions

---
+++ pierian_data_python/lambda.py
@@ -0,0 +1,13 @@
+# lambda function is lambda argument: expresion
+def square(x):
+    print(lambda x: x+5)
+square(10)
+#square([5,10])
+
+# map map() and filter filter()
+
+def numbers(a):
+    filter(lambda x: x>10, map(lambda x: x * 5))
+results=numbers([2,3,4,5,6,,7,])
+print(results)
+
