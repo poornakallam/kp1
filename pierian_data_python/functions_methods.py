76a8788cefce2bd66d64001c8bdde61fd680a385
Poorna Kallam <2233826@tcs.com>
2025-09-16 21:35:00 +0530

    homework functions

--- pierian_data_python/functions_methods.py
+++ pierian_data_python/functions_methods.py
@@ -2,3 +2,7 @@ def vol(redies):
     return 4/3*22/7*redies
 results=vol(21)
 print(results)
+
+def ran_check(num,low,high):
+    print(f'{num} in the range between {low} and {high}')
+ran_check(3,1,9)
