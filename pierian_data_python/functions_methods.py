e03a457af6e015e411cad1b836230b4841aae729
Poorna Kallam <2233826@tcs.com>
2025-09-17 20:59:51 +0530

    homework functions

--- pierian_data_python/functions_methods.py
+++ pierian_data_python/functions_methods.py
@@ -11,3 +11,14 @@ def ran_check(num,low,high):
         print(False)     
 ran_check(9,1,3)
 
+def up_low(s):
+    s = "Hello Mr. Rogers, how are you this fine Tuesday?"
+    for i in s:
+        if i == s.upper():
+            print(f'upper letter count: {i}')
+        elif i == s.lower():
+            print(f'lower letter count: {i}')
+        else:
+            print("nothing")
+up_low(s)
+
